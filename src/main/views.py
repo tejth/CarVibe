348301a3b2e605607a32f6112d2041471b8169e2
tejth <ptejendra91@gmail.com>
2025-02-09 11:39:01 +0530

    Creating first Template

--- src/main/views.py
+++ src/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def main_view(request):
-    return HttpResponse("<h1>Welcome to CarVibe</h1>")
+    return render(request, "views/main.html",{"name":"CarVibe"})
 
     
     
