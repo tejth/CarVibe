fefc39bf959ed38020bc1fa525034df5da38302a
tejth <ptejendra91@gmail.com>
2025-02-10 20:50:06 +0530

    making urls of profile photos workable

--- src/CarVibe/settings.py
+++ src/CarVibe/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Media Files(uploaded)
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
