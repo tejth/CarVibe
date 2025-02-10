776695f628e96c6faebe899f90350d32ffebef7f
tejth <ptejendra91@gmail.com>
2025-02-10 07:25:35 +0530

    authentication part one creating user model and applying migrations

---
+++ src/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+ user = models.OneToOneField(User, on_delete=models.CASCADE) 
+ photo = models.ImageField(null=True)
+ bio = models.CharField(max_length = 140 , blank=True)
+ phone_number = models.CharField(max_length=12 , blank=True)
