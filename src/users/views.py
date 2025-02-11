2a8b06d458e969c3833a761e882e125fed16a10e
tejth <ptejendra91@gmail.com>
2025-02-11 13:20:57 +0530

    completed base of login view

--- src/users/views.py
+++ src/users/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.shortcuts import render
 
 # Create your views here.
 
 def login_view(request):
-    return HttpResponse("Login View ")
+    return render(request , 'views/login.html')
 
