8fd3e3f58b41503187536948b60a3c279d96667a
tejth <ptejendra91@gmail.com>
2025-02-13 17:57:51 +0530

    creating view page to showcase cars using url parameters

--- src/main/views.py
+++ src/main/views.py
@@ -48,3 +48,14 @@ def list_view(request):
         location_form = LocationForm()
     return render(request , 'views/list.html',{'listing_form': listing_form, 'location_form': location_form,})
 
+@login_required
+def listing_view(request,id):
+    try:
+     listing = Listing.objects.get(id=id)
+     if listing is None:
+         raise Exception
+     return render(request , 'views/listing.html',{})
+    except Exception as e:
+        messages.error(request, f'Invalid UID {id} was provided for listing')
+        return redirect('home')
+   
